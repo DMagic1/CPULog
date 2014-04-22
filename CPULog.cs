a0be838fc972b10e8fde5b68f80941d0a0315933
DMagic <david.grandy@gmail.com>
2014-04-22 00:56:56 -0400

    Initial Commit

---
+++ CPULog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.IO;
+using System.Timers;
+
+namespace CPULog
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class CPULog : MonoBehaviour
+    {
+        private int updatedFrameCount = 0;
+        private float time = 0;
+
+        ConfigNode node = new ConfigNode();
+        string path = Path.Combine(new DirectoryInfo(KSPUtil.ApplicationRootPath).FullName, "GameData/CPULog.txt").Replace("\\", "/");
+
+        public void Start()
+        {
+            System.Timers.Timer cTimer = new System.Timers.Timer(1000);
+            cTimer.Elapsed += new ElapsedEventHandler(onTimerEvent);
+            cTimer.Enabled = true;
+        }
+
+        private void onTimerEvent(object source, ElapsedEventArgs e)
+        {
+            
+            int frame = Time.frameCount;
+            time += 1; 
+            int fps = frame - updatedFrameCount;
+            updatedFrameCount = frame;
+            saveToFile(time, fps);
+        }
+        
+        private void saveToFile(float time, int entry)
+        {
+            node.AddValue(time.ToString(), entry.ToString());
+            if (node.Save(path)) print("values saved");
+        }
+    }
+}
