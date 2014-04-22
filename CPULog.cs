e0a45ff85f650391efacf53865cd4194b7f9d487
David Grandy <david.grandy@gmail.com>
2014-04-22 16:29:15 -0400

    Prevent breakage

--- CPULog.cs
+++ CPULog.cs
@@ -1,43 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 using System.IO;
 using System.Timers;
 
 namespace CPULog
 {
     [KSPAddon(KSPAddon.Startup.Flight, false)]
-    public class CPULog : MonoBehaviour
+    class CPULog : MonoBehaviour
     {
         private int updatedFrameCount = 0;
         private float time = 0;
 
+        Timer cTimer;
         ConfigNode node = new ConfigNode();
         string path = Path.Combine(new DirectoryInfo(KSPUtil.ApplicationRootPath).FullName, "GameData/CPULog.txt").Replace("\\", "/");
 
-        public void Start()
+        private void Start()
         {
-            System.Timers.Timer cTimer = new System.Timers.Timer(1000);
+            cTimer = new System.Timers.Timer(1000);
             cTimer.Elapsed += new ElapsedEventHandler(onTimerEvent);
             cTimer.Enabled = true;
         }
 
         private void onTimerEvent(object source, ElapsedEventArgs e)
-        {
-            
+        {            
             int frame = Time.frameCount;
             time += 1; 
             int fps = frame - updatedFrameCount;
             updatedFrameCount = frame;
             saveToFile(time, fps);
+            if (node.CountValues > 2000)
+            {
+                node.ClearValues();
+                time = 0;
+                updatedFrameCount = 0;
+            }
         }
         
         private void saveToFile(float time, int entry)
         {
             node.AddValue(time.ToString(), entry.ToString());
             if (node.Save(path)) print("values saved");
+
+        }
+
+        private void OnDestroy()
+        {
+            cTimer.Stop();
         }
     }
 }
