708758aa4ab0e3513fb942311a3d16b6d11baf3d
DMagic <david.grandy@gmail.com>
2014-04-22 23:39:42 -0400

    Ready, almost?

--- CPULog.cs
+++ CPULog.cs
@@ -8,7 +8,7 @@ namespace CPULog
     class CPULog : MonoBehaviour
     {
         private int updatedFrameCount = 0;
-        private float time = 0;
+        private int time = 0;
 
         Timer cTimer;
         ConfigNode node = new ConfigNode();
@@ -16,36 +16,44 @@ namespace CPULog
 
         private void Start()
         {
-            cTimer = new System.Timers.Timer(1000);
-            cTimer.Elapsed += new ElapsedEventHandler(onTimerEvent);
-            cTimer.Enabled = true;
+            if (FlightGlobals.ActiveVessel.name == "Mark1-2Pod (CPU Test 600 v23_5)") GameEvents.onStageActivate.Add(TimerStart);
+        }
+
+        private void TimerStart(int stage)
+        {
+            if (stage == 14)
+            {
+                print("Starting Timer");
+                GameEvents.onStageActivate.Remove(TimerStart);
+                node.ClearValues();
+                node.AddValue("Begin Run", System.DateTime.Now.ToString());
+                cTimer = new System.Timers.Timer(1000);
+                cTimer.Elapsed += new ElapsedEventHandler(onTimerEvent);
+                cTimer.Enabled = true;
+            }
+            else GameEvents.onStageActivate.Remove(TimerStart);
         }
 
         private void onTimerEvent(object source, ElapsedEventArgs e)
-        {            
+        {
             int frame = Time.frameCount;
             time += 1; 
             int fps = frame - updatedFrameCount;
             updatedFrameCount = frame;
             saveToFile(time, fps);
-            if (node.CountValues > 2000)
-            {
-                node.ClearValues();
-                time = 0;
-                updatedFrameCount = 0;
-            }
         }
         
-        private void saveToFile(float time, int entry)
+        private void saveToFile(int time, int entry)
         {
             node.AddValue(time.ToString(), entry.ToString());
-            if (node.Save(path)) print("values saved");
-
+            node.Save(path);
         }
 
         private void OnDestroy()
         {
+            print("Destroying Timer");
             cTimer.Stop();
+            GameEvents.onStageActivate.Remove(TimerStart);
         }
     }
 }
