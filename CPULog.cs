2e1d39a92610a4808a2971b143ecc24d6448a2af
DMagic <david.grandy@gmail.com>
2014-07-19 14:45:56 -0400

    Update for KSP 0.24
    vessel.name is apparently changed
    use vessel.vesselname instead

--- CPULog.cs
+++ CPULog.cs
@@ -1,4 +1,35 @@
-﻿using UnityEngine;
+﻿/* CPULog
+ * Benchmarking program designed for use with CPU test rocket
+ * 
+ * See here for more details: http://forum.kerbalspaceprogram.com/threads/42877-CPU-Performance-Database
+ *
+ * Copyright (c) 2014, David Grandy <david.grandy@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used 
+ * to endorse or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ */
+
+using UnityEngine;
 using System.IO;
 using System.Timers;
 
@@ -16,14 +47,18 @@ namespace CPULog
 
         private void Start()
         {
-            if (FlightGlobals.ActiveVessel.name == "Mark1-2Pod (CPU Test 600 v23_5)") GameEvents.onStageActivate.Add(TimerStart);
+            if (FlightGlobals.ActiveVessel.vesselName == "CPU Test 600 v23_5") {
+                print("[CPULogger] CPU test rocket found");
+                GameEvents.onStageActivate.Add(TimerStart);
+            }
+            else print("[CPULogger] No CPU test rocket found; shutting down");
         }
 
         private void TimerStart(int stage)
         {
             if (stage == 14)
             {
-                print("Starting Timer");
+                print("[CPULogger] Starting timer");
                 GameEvents.onStageActivate.Remove(TimerStart);
                 node.ClearValues();
                 node.AddValue("Begin Run", System.DateTime.Now.ToString());
@@ -51,7 +86,7 @@ namespace CPULog
 
         private void OnDestroy()
         {
-            print("Destroying Timer");
+            print("[CPULogger] Shutting down");
             cTimer.Stop();
             GameEvents.onStageActivate.Remove(TimerStart);
         }
